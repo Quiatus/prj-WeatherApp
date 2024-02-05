42d1519c549fa48872707d5d083969ccc59b1d88
Quiatus <Quiatus@gmail.com>
2024-02-05 17:42:08 +0100

    remove API key

--- dist/js/dataFunctions.js
+++ dist/js/dataFunctions.js
@@ -1,4 +1,4 @@
-const WEATHER_API_KEY = "5deae395ce8e574bb065123efbef16e0";
+const WEATHER_API_KEY = "";
 
 // locationObj is an instance of class Current Locaton. coordsObj passes teh current location passed from main function
 export const setLocationObject = (locationObj, coordsObj) => {
